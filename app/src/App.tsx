5f8b8daa2b7ddfe898d22e515079ebf2933fe948
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2020-11-11 17:59:51 +0500

    React&ts initialize

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header">
+        <button>Custom math parser</button>
+        <button>Wolfram</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
